a753646d85a7ab7d48c9c490a62af7609fbe90bf
GabrielVictor07 <bielmottagit@gmail.com>
2025-04-28 18:13:09 -0300

    react

--- my-app/app/(tabs)/index.tsx
+++ my-app/app/(tabs)/index.tsx
@@ -1,80 +1,121 @@
-import react, { Component } from 'react';
-import {View, Text, Image} from 'react-native';
+import React, { Component } from 'react';
+import { View, Text, TextInput, TouchableOpacity, Alert, StyleSheet } from 'react-native';
 
-/* Hello Word, Styles
-class App extends Component{
-  render(){
-    const info = 'Site de Gabriel Motta';
-    return(
-      <View>
-          <Text style={{color: 'red', fontSize: 18, padding: 20, backgroundColor: 'White', marginTop: 30, textAlign: 'center'}}>
-            Hello Word!
-          </Text>
-          <Image
-          // colocar imagem no react, colocar(uri: 'IMAGEM---') 
-          source={{uri: ""}}/>
-          <Text style={{color: 'white', backgroundColor: 'red', borderRadius: 20, textAlign: 'center', padding: 20, fontSize: 20, width: 300, margin: 'auto',}}>
-            {info}
-          </Text>
-          
-      </View>
-    )
+type State = {
+  userInput: string;
+  secretNumber: number;
+  attempts: number;
+};
+
+class JogoDoNumero extends Component<{}, State> {
+  constructor(props: {}) {
+    super(props);
+    this.state = {
+      userInput: '',
+      secretNumber: this.generateSecretNumber(),
+      attempts: 1,
+    };
   }
-}
 
-export default App;
-*/
+  generateSecretNumber = () => {
+    return Math.floor(Math.random() * 10) + 1;
+  };
 
-/* funcion
-// funcion para renderizar um texto dentro do view
-function app(){
+  handleGuess = () => {
+    const {userInput, secretNumber, attempts }  = this.state;
+    const guess = parseInt(userInput);
+
+  if (isNaN(guess)){
+    Alert.alert('Erro', 'Por favor, digite um numero valido');
+    return;
+  }
+  if (guess === secretNumber) {
+    Alert.alert('Parabéns!',
+        `Você acertou o número ${secretNumber} com ${attempts + 1} tentativa(s)!`
+      );
+      this.setState({
+        userInput: '',
+        secretNumber: this.generateSecretNumber(),
+        attempts: 0,
+      });
+  } else {
+    Alert.alert('Errou!', 'Tente novamente!');
+    this.setState({ attempts: attempts + 1 });
+  }
+
+  // Limpa o campo
+  this.setState({ userInput: '' });
+  };
+
+render() {
   return (
-    <View>
-      <Text>
-          Olá React Native!
-      </Text>
-      <p>Brasil</p>
-    </View>
-  );
-}
-export default app;
-*/
-
-type Props = {
-  largura: number;
-  altura: number;
-}
-
-class App extends Component {
-  render (){
-    return (
-      // chamando componente Uninassau e atribuindo novas props
-
-      <View>
-        <Text style={{color: 'white'}}>
-          Olá Galera!
-        </Text>
-
-        <Filme largura={300} altura={300}/>
-      </View>
-    )
-  }
-}
-
-export default App;
-
-class Filme extends Component <Props>{
-  // criando o proprio componente
-
-  render(){
-    // declarando variavel para imagem
-    const img = 'https://en.wikipedia.org/wiki/File:2019_Toyota_Corolla_Icon_Tech_VVT-i_Hybrid_1.8.jpg'
-    // O que vai Retornar
-    return(
-      <Image // colocando img 
-        source={{uri: img}} // obrigatorio colocar largura e altura
-        style={{width: this.props.largura, height: this.props.altura}}
+    <View style={styles.container}>
+      <Text style={styles.title}>Adivinha o numero Secreto 1 a 10</Text>
+      <TextInput
+        style={styles.input}
+        placeholder="digite seu palpite"
+        value={this.state.userInput}
+        onChangeText={(text) => this.setState({ userInput: text })}
       />
+      <TouchableOpacity
+        style={styles.btn}
+        onPress={this.handleGuess}>
+          <Text style={styles.buttonText}>Enviar</Text>
+      </TouchableOpacity>
+      <Text style={styles.attemptsText}>
+          Tentativas: {this.state.attempts}
+        </Text>
+    </View>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 20,
+    marginTop: 40,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  title: {
+    color: 'white',
+    fontSize: 20,
+    marginBottom: 20,
+    textAlign: 'center',
+    backgroundColor: 'blue',
+    padding: 10,
+    borderRadius: 10
+  },
+  input: {
+    width: 300,
+    height: 40,
+    borderColor: 'white',
+    borderWidth: 1,
+    marginBottom: 15,
+    paddingHorizontal: 10,
+    borderRadius: 10,
+    color: 'white',
+    backgroundColor: 'transparente'
+  },
+  btn: {
+    backgroundColor: 'white',
+    padding: 20,
+    width: 200,
+    borderRadius: 20
+  },
+  buttonText: {
+    color: 'black',
+    textAlign: 'center',
+    fontWeight: 500
+  },
+  attemptsText: {
+    marginTop: 20,
+    textAlign: 'center',
+    fontSize: 18,
+    fontWeight: 'bold',
+    color: 'white'
+  },
+});
+
+export default JogoDoNumero;
